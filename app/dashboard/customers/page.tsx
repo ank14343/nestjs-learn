eac051658821ce41acb13142a847c67776148e05
Ankur Arya <akarya009@gmail.com>
2024-01-18 15:14:43 +0530

    force delay added for "/dashboard/invoices" and "/dashboard/customers"

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,4 @@
-export default function CustomerPage() {
+export default async function CustomerPage() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   return <p>Customer Page</p>;
 }
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,4 @@
-export default function InvoicePage() {
+export default async function InvoicePage() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   return <p>Invoice Page</p>;
 }
